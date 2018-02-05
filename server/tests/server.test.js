d50ef67bc99eb87410e783872d5db5993db7d170
Martin Simango <Martin@Martins-MacBook-Pro.local>
2018-02-05 22:58:13 +0200

    added tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,18 @@ const supertest= require('supertest');
 var {Todo}= require("../models/todo");
 var {app}= require("../server");
 
+const todos=[{
+text:"First test todo"
+},{
+text:"Second test todo"
+},{
+text:"Third test todo"
+}]
 beforeEach((done)=>{
-Todo.remove({}).then(()=>done());
-})
-
+Todo.remove({}).then(()=>{
+   return Todo.insertMany(todos);  //return a promise
+}).then(()=>done());
+});
 describe("POST /todos",()=>{
 
 it("Should create a new todo",(done)=>{ //done because its calling an asynchronous function
@@ -23,7 +31,7 @@ supertest(app)
     if(err){
         return done(err);
     }
-    Todo.find().then((todos)=>{ //check the database
+    Todo.find({text}).then((todos)=>{ //check the database
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
         done();
@@ -42,11 +50,28 @@ it("Should not create todo",(done)=>{
      if(err){
          return done(err);
      }
-     Todo.find().then((todos)=>{
-         expect(todos.length).toBe(0);
+     Todo.find().then((todos)=>{ 
+         expect(todos.length).toBe(3);
          done();
      }).catch((e)=>done(e))
  })
 
 })
+})
+
+describe("GET /todos",()=>{
+
+it("Should get all todos",(done)=>{
+supertest(app)
+.get("/todos")
+.expect(200)
+.expect((res)=>{
+ expect(res.body.todos.length).toBe(3);
+})
+.end(done);
+
+})
+
+
+
 })
