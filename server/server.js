ce1b4a7c77a868e5c8d1f00c5c1a92286f3329d0
Martin Simango <Martin@Martins-MacBook-Pro.local>
2018-02-05 17:36:40 +0200

    added POST /todo route and refractored mongoose

--- server/server.js
+++ server/server.js
@@ -1,62 +1,26 @@
-var mongoose=require('mongoose');
-
-mongoose.Promise=global.Promise; //use the javasrcipt promise library
-//connect to the mongo database
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
-//model specifies what attrivutes a doc in the model(document) can have
-var Todo= mongoose.model('Todo',{ //Todo is the name of the collection
-    text:{
-        type:String,
-        required:true,
-        minlength:1,
-        trim:true 
-    },
-    completed:{
-        type:Boolean,
-        default: false
-    },
-    completedAt:{
-        type: Number,
-        default:null
-    }
-});
-
-// var newTodo= new Todo({
-//     text: 'Cook Dinner'
-// });
-
-// newTodo.save().then((doc)=>{
-// console.log("Saved the todo",doc);
-// },(err)=>{
-// console.log("Unable to save todo",err);
-// });
-
-var newTodo_2 = new Todo({
-    text: 'Open the Door'
-});
-
-// newTodo_2.save().then((doc)=>{
-// console.log("Saved the todo",doc);
-// },(err)=>{
-// console.log("Unable to save todo",err);
-// });
+var express = require('express');
+var bodyParser= require('body-parser');
 
 
-var User= mongoose.model('User',{
-    email:{
-        type:String,
-        required:true,
-        minlength:1,
-        trim:true
-    }
-})
+var {mongoose}=require('./db/mongoose');
+var {Todo}=require("./models/todo");
+var {User}=require("./models/user");
 
-var newUser= new User({
-    email:"shukosimango@yahoo.com"
-})
-newUser.save().then((doc)=>{
-console.log("Saved the todo",doc);
+var app=express();
+
+app.use(bodyParser.json());
+app.post('/todos',(req,res)=>{ //handle a post request at the route /todos
+var todo= new Todo({
+    text: req.body.text
+}); 
+todo.save().then((doc)=>{
+res.send(doc); //
 },(err)=>{
-console.log("Unable to save todo",err);
+res.status(400).send(err);
+
+})
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running on port",3000);
 });
