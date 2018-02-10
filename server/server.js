016ff9417b77058712ebaf434336019762dc9fb8
Martin Simango <Martin@Martins-MacBook-Pro.local>
2018-02-11 01:01:20 +0200

    added get id route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var bodyParser= require('body-parser');
 
 
 var {mongoose}=require('./db/mongoose');
+var {ObjectID}= require('mongodb'); //for validating an ID
 var {Todo}=require("./models/todo");
 var {User}=require("./models/user");
 
@@ -20,7 +21,23 @@ res.status(400).send(err);
 
     });
 })
-
+app.get("/todos/:id",(req,res)=>{
+    var id= req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(400).send();
+    }
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+          return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((err)=>{
+        res.send(400).send();
+    })
+    //validate id
+        //ssend 404 if not valid
+    
+})
 app.get("/todos",(req,res)=>{
 Todo.find().then((todos)=>{
     res.send({todos});
